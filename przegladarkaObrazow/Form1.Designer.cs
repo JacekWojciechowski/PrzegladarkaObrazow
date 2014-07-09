474cbcf1a4acd2a7e64a2a2d00531a29e2c3d8a6
JacekWojciechowski <jacaa321@gmail.com>
2014-07-09 19:05:40 +0200

    Przeglądarka obrazów

---
+++ przegladarkaObrazow/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace przegladarkaObrazow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPoprzednie = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboCzas = new System.Windows.Forms.ComboBox();
+            this.btnPokazSlajdow = new System.Windows.Forms.Button();
+            this.btnObrocPrawo = new System.Windows.Forms.Button();
+            this.btnObrotLewo = new System.Windows.Forms.Button();
+            this.btnNastepne = new System.Windows.Forms.Button();
+            this.btnWczytaj = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPoprzednie
+            // 
+            this.btnPoprzednie.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPoprzednie.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPoprzednie.Enabled = false;
+            this.btnPoprzednie.Image = global::przegladarkaObrazow.Properties.Resources.previous;
+            this.btnPoprzednie.Location = new System.Drawing.Point(378, 17);
+            this.btnPoprzednie.Name = "btnPoprzednie";
+            this.btnPoprzednie.Size = new System.Drawing.Size(75, 36);
+            this.btnPoprzednie.TabIndex = 2;
+            this.btnPoprzednie.UseVisualStyleBackColor = true;
+            this.btnPoprzednie.Click += new System.EventHandler(this.btnPoprzednie_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Linen;
+            this.panel1.CausesValidation = false;
+            this.panel1.Controls.Add(this.comboCzas);
+            this.panel1.Controls.Add(this.btnPokazSlajdow);
+            this.panel1.Controls.Add(this.btnObrocPrawo);
+            this.panel1.Controls.Add(this.btnObrotLewo);
+            this.panel1.Controls.Add(this.btnNastepne);
+            this.panel1.Controls.Add(this.btnWczytaj);
+            this.panel1.Controls.Add(this.btnPoprzednie);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel1.Location = new System.Drawing.Point(0, 545);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 65);
+            this.panel1.TabIndex = 4;
+            // 
+            // comboCzas
+            // 
+            this.comboCzas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboCzas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboCzas.FormattingEnabled = true;
+            this.comboCzas.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboCzas.Location = new System.Drawing.Point(702, 19);
+            this.comboCzas.Name = "comboCzas";
+            this.comboCzas.Size = new System.Drawing.Size(50, 32);
+            this.comboCzas.TabIndex = 6;
+            this.comboCzas.Text = "5";
+            this.comboCzas.Visible = false;
+            this.comboCzas.SelectedIndexChanged += new System.EventHandler(this.comboCzas_SelectedIndexChanged);
+            // 
+            // btnPokazSlajdow
+            // 
+            this.btnPokazSlajdow.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPokazSlajdow.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPokazSlajdow.Enabled = false;
+            this.btnPokazSlajdow.Image = global::przegladarkaObrazow.Properties.Resources.play;
+            this.btnPokazSlajdow.Location = new System.Drawing.Point(621, 17);
+            this.btnPokazSlajdow.MaximumSize = new System.Drawing.Size(400, 200);
+            this.btnPokazSlajdow.Name = "btnPokazSlajdow";
+            this.btnPokazSlajdow.Size = new System.Drawing.Size(75, 36);
+            this.btnPokazSlajdow.TabIndex = 5;
+            this.btnPokazSlajdow.UseVisualStyleBackColor = true;
+            this.btnPokazSlajdow.Click += new System.EventHandler(this.btnPokazSlajdow_Click);
+            // 
+            // btnObrocPrawo
+            // 
+            this.btnObrocPrawo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnObrocPrawo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnObrocPrawo.Enabled = false;
+            this.btnObrocPrawo.Image = global::przegladarkaObrazow.Properties.Resources.rotateRight;
+            this.btnObrocPrawo.Location = new System.Drawing.Point(297, 17);
+            this.btnObrocPrawo.Name = "btnObrocPrawo";
+            this.btnObrocPrawo.Size = new System.Drawing.Size(75, 36);
+            this.btnObrocPrawo.TabIndex = 1;
+            this.btnObrocPrawo.UseVisualStyleBackColor = true;
+            this.btnObrocPrawo.Click += new System.EventHandler(this.btnObrocPrawo_Click);
+            // 
+            // btnObrotLewo
+            // 
+            this.btnObrotLewo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnObrotLewo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnObrotLewo.Enabled = false;
+            this.btnObrotLewo.Image = global::przegladarkaObrazow.Properties.Resources.rotateLeft;
+            this.btnObrotLewo.Location = new System.Drawing.Point(216, 17);
+            this.btnObrotLewo.Name = "btnObrotLewo";
+            this.btnObrotLewo.Size = new System.Drawing.Size(75, 36);
+            this.btnObrotLewo.TabIndex = 0;
+            this.btnObrotLewo.UseVisualStyleBackColor = true;
+            this.btnObrotLewo.Click += new System.EventHandler(this.btnObrotLewo_Click);
+            // 
+            // btnNastepne
+            // 
+            this.btnNastepne.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNastepne.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNastepne.Enabled = false;
+            this.btnNastepne.Image = global::przegladarkaObrazow.Properties.Resources.next;
+            this.btnNastepne.Location = new System.Drawing.Point(540, 17);
+            this.btnNastepne.Name = "btnNastepne";
+            this.btnNastepne.Size = new System.Drawing.Size(75, 36);
+            this.btnNastepne.TabIndex = 4;
+            this.btnNastepne.UseVisualStyleBackColor = true;
+            this.btnNastepne.Click += new System.EventHandler(this.btnNastepne_Click);
+            // 
+            // btnWczytaj
+            // 
+            this.btnWczytaj.AccessibleDescription = "";
+            this.btnWczytaj.AccessibleName = "";
+            this.btnWczytaj.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnWczytaj.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnWczytaj.Image = global::przegladarkaObrazow.Properties.Resources.pictures;
+            this.btnWczytaj.Location = new System.Drawing.Point(459, 17);
+            this.btnWczytaj.Name = "btnWczytaj";
+            this.btnWczytaj.Size = new System.Drawing.Size(75, 36);
+            this.btnWczytaj.TabIndex = 3;
+            this.btnWczytaj.Tag = "";
+            this.btnWczytaj.UseVisualStyleBackColor = true;
+            this.btnWczytaj.Click += new System.EventHandler(this.btnWczytaj_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.CausesValidation = false;
+            this.panel2.Controls.Add(this.pictureBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1008, 545);
+            this.panel2.TabIndex = 7;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1008, 545);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 610);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Przeglądarka Zdjęć";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Klawiatura_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNastepne;
+        private System.Windows.Forms.Button btnWczytaj;
+        private System.Windows.Forms.Button btnObrotLewo;
+        private System.Windows.Forms.Button btnObrocPrawo;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPokazSlajdow;
+        private System.Windows.Forms.ComboBox comboCzas;
+        private System.Windows.Forms.Button btnPoprzednie;
+    }
+}
+
---
+++ przegladarkaObrazow/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace przegladarkaObrazow
+{
+
+   
+    public partial class Form1 : Form
+    {
+        private string plik;
+        public Form1(string []arg)
+        {            
+            InitializeComponent();
+            foreach(string s in arg)      //zapisuje ścieżkę do otwieranego pliku
+                plik += s;
+        }
+
+
+
+        public class Wczytaj
+        {
+            public  int licznikObrotu = 0;  //liczy ile razy obrócono zdjęcie
+            private  List<string> lista;     //lista plików z obecnego folderu
+            private int obecny;             //numer aktualnego pliku
+            private DirectoryInfo pobierz;  //informacje o katalogu, w którym znajduje się otwarty plik
+            private PictureBox box;         //picturebox, w którym wyświetlane jest zdjęcie
+            private string sciezka;         //zmienna do przechowywania aktualnej sciezki do pliku
+            
+            //Właściwość pobierająca nazwę obecnego pliku do wyświetlenia w Form1.Text
+            public string Tytul
+            {
+                get
+                {
+                    return lista[obecny];
+                }
+                
+            }
+        
+            //wczytywanie listy plików bieżącego katalogu
+            private void wczytajListe(OpenFileDialog otworz)
+            {
+                lista = new List<string>();
+                pobierz = new DirectoryInfo(Path.GetDirectoryName(otworz.FileName).ToString());
+                foreach (var fi in pobierz.EnumerateFiles("*.jpg")  //dopisywanie do listy plików rozszerzeń
+                    .Concat(pobierz.EnumerateFiles("*.png"))
+                    .Concat(pobierz.EnumerateFiles("*.tif"))
+                    .Concat(pobierz.EnumerateFiles("*.bmp"))
+                    .Concat(pobierz.EnumerateFiles("*.jpeg")))   
+                {
+                    lista.Add(fi.Name.ToString());      
+                }
+                for (int a = 0; a < lista.Count; a++) //pętla, która wyszukuje obecny plik i zapisuje jego indeks z listy
+                {
+                    if (lista[a] == otworz.SafeFileName)
+                        obecny = a;
+                }
+            }
+           
+            //wyświetlenie aktualnego pliku
+            public void aktualny(PictureBox ten)
+            {
+                box = ten;
+                OpenFileDialog otworz = new OpenFileDialog();
+                otworz.InitialDirectory = sciezka;
+                otworz.Filter = "Obrazy|*.jpg;*.jpeg;*.png;*.tif;*.bmp" ;
+                if (otworz.ShowDialog() == DialogResult.OK)
+                {                   
+                    wczytajListe(otworz);
+                    sciezka = pobierz.FullName;
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                }
+             }
+
+            //wyświetlenie pliku wybranego poza programem, np. w eksploratorze windows
+            public void aktualnyWindows(string s, PictureBox ten)
+            {
+                sciezka = Path.GetDirectoryName(s);
+                box = ten;
+                OpenFileDialog otworz = new OpenFileDialog();
+                otworz.FileName = s;              
+                wczytajListe(otworz);                
+                box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                
+                
+            }           
+
+            //wyświetlenie następnego pliku z wczytanej listy
+            public void nastepny()
+            {
+                if (obecny < lista.Count - 1)
+                {
+                    obecny++;
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                }
+                else
+                {
+                    obecny = 0;
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                }
+            }
+
+            //wczytanie poprzedniego pliku z wczytanej listy
+            public void poprzednie()
+            {
+                if (obecny > 0)
+                {
+                    obecny--;
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                }
+                else
+                {
+                    obecny = lista.Count-1;
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                }
+            }
+
+            //obrócenie obrazu w lewo
+            public void obrocLewo()
+            {                
+                if (licznikObrotu == 0)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    licznikObrotu++;
+                }
+                else if (licznikObrotu == 1)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    licznikObrotu++;
+                }
+                else if (licznikObrotu == 2)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    licznikObrotu++;
+                }
+                else
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    licznikObrotu = 0;
+                }    
+            }
+
+            //obrócenie obrazu w prawo
+            public void obrocPrawo()
+            {
+                if (licznikObrotu == 0)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    licznikObrotu = 3;
+                }
+                else if (licznikObrotu == 1)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);                   
+                    licznikObrotu = 0; ;
+                }
+                else if (licznikObrotu == 2)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    licznikObrotu = 1;
+                }
+                else if (licznikObrotu == 3)
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    box.Image.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    licznikObrotu = 2;
+                }
+                else
+                {
+                    box.Image = Image.FromFile(pobierz.FullName + "\\" + lista[obecny]);
+                    licznikObrotu = 0;
+                }
+            }
+            
+        }
+
+        //POCZĄTEK PROGRAMU
+
+        Wczytaj obraz = new Wczytaj();  //tworzymy obiekt klasy Wczytaj
+        bool PokazWlaczony = false;     // zmienna, która sprawdza, czy pokaz slajdów jest wł/wył
+        
+
+        private void btnWczytaj_Click(object sender, EventArgs e)
+        {
+            obraz.licznikObrotu = 0;
+            obraz.aktualny(pictureBox);
+            btnNastepne.Enabled = true;
+            btnPoprzednie.Enabled = true;
+            btnObrotLewo.Enabled = true;
+            btnObrocPrawo.Enabled = true;
+            btnPokazSlajdow.Enabled = true;
+            this.Text = obraz.Tytul + " - Przegladarka Zdjęć";
+        }
+
+
+        private void btnNastepne_Click(object sender, EventArgs e)
+        {
+            obraz.licznikObrotu = 0;
+            obraz.nastepny();
+            this.Text = obraz.Tytul + " - Przegladarka Zdjęć";
+        }
+
+        private void btnPoprzednie_Click(object sender, EventArgs e)
+        {
+            obraz.licznikObrotu = 0;
+            obraz.poprzednie();
+            this.Text = obraz.Tytul + " - Przegladarka Zdjęć";
+        }
+
+        private void btnObrotLewo_Click(object sender, EventArgs e)
+        {
+            obraz.obrocLewo();
+        }
+
+        private void btnObrocPrawo_Click(object sender, EventArgs e)
+        {
+            obraz.obrocPrawo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (plik != null)    //sprawdzamy, czy nie otwarto pliku poza programem, np. eksplorator windows
+            {
+                obraz.licznikObrotu = 0;
+                btnNastepne.Enabled = true;
+                btnPoprzednie.Enabled = true;
+                btnObrotLewo.Enabled = true;
+                btnObrocPrawo.Enabled = true;
+                btnPokazSlajdow.Enabled = true;
+                obraz.aktualnyWindows(plik, pictureBox);
+                this.Text = obraz.Tytul + " - Przegladarka Zdjęć";
+            }              
+         }        
+
+        //metoda do obługi klawiatury - zdarzenie z Form1_KeyDown
+        private void Klawiatura_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    panel2.Focus();
+                    btnNastepne_Click(sender, e);
+                }
+                else  if (e.KeyCode == Keys.Oemcomma)
+                {    
+                    btnPoprzednie_Click(sender, e);
+                    btnPoprzednie.Focus();
+                    
+                }
+                else if (e.KeyCode == Keys.OemPeriod)
+                {
+                    btnNastepne_Click(sender, e);
+                    btnNastepne.Focus();
+                }
+                else if (e.KeyCode == Keys.N)
+                {
+                    btnObrotLewo_Click(sender, e);
+                    btnObrotLewo.Focus();
+                }
+                else if (e.KeyCode == Keys.M)
+                {
+                    btnObrocPrawo_Click(sender, e);
+                    btnObrocPrawo.Focus();
+                }
+                else if (e.KeyCode == Keys.Enter)
+                {
+                    btnWczytaj.Focus();
+                    btnWczytaj_Click(sender, e);
+                }
+                else if (e.KeyCode == Keys.ShiftKey)
+                    btnPokazSlajdow_Click(sender, e);
+                else if (e.KeyCode == Keys.Escape)
+                    Application.Exit();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Musisz najpierw wczytać zdjęcie!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+
+        }
+
+        //Timer do pokazu slajdów
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            obraz.licznikObrotu = 0;
+            obraz.nastepny();
+            this.Text = obraz.Tytul + " - Przegladarka Zdjęć";
+        }
+
+        private void btnPokazSlajdow_Click(object sender, EventArgs e)
+        { 
+            if (!PokazWlaczony)
+            {                
+                comboCzas.Visible = true;
+                comboCzas.Focus();
+                btnPokazSlajdow.Image = global::przegladarkaObrazow.Properties.Resources.stop;
+                timer1.Interval = Convert.ToInt32(comboCzas.Text)*1000;
+                timer1.Enabled = true;
+                PokazWlaczony = true;
+            }
+            else
+            {
+                comboCzas.Visible = false;
+                btnPokazSlajdow.Image = global::przegladarkaObrazow.Properties.Resources.play;
+                timer1.Enabled = false;
+                PokazWlaczony = false;
+            }
+        }
+
+        //zmiana czasu wyświetlania zdjęcia w comboBoxsie
+        private void comboCzas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(comboCzas.Text) * 1000;
+        }
+
+        
+     
+        
+
+
+        
+
+       
+
+      
+
+
+        
+
+     
+
+       
+
+
+       
+    }
+}
